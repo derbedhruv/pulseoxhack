1babb01a1ad5920fe1149fc59c01a0d22aa11b00
Alankar Kotwal <alankarkotwal13@gmail.com>
2014-12-01 17:38:55 +0530

    finished

--- pulseox_decode.py
+++ pulseox_decode.py
@@ -28,6 +28,11 @@
 import 	GPIO_Intel as GPIO
 import time, sys, os
 
+# Define the output
+spo2Tens  = 0
+spo2Units = 0
+spo2      = 0
+
 # Create a GPIO object
 io = GPIO.Intel()
 
@@ -43,4 +48,43 @@ io.setup('IO9', 'in')
 io.setup('IO10', 'in')
 io.setup('IO11', 'in')
 
+# Setup pin dictionary
+pins = {'CA': 'IO2',
+		'CB': 'IO3',
+		'CC': 'IO4',
+		'Aa': 'IO5',
+		'Ab': 'IO6',
+		'Ac': 'IO7',
+		'Ad': 'IO8',
+		'Ae': 'IO9',
+		'Af': 'IO10',
+		'Ag': 'IO11'}
+
+# Setup a 7-segment to binary dictionary
+conv = {0b1110111: 0,
+		0b0100100: 1,
+		0b1011101: 2,
+		0b1101101: 3,
+		0b0101110: 4,
+		0b1101011: 5,
+		0b1111011: 6,
+		0b0100101: 7,
+		0b1111111: 8,
+		0b1101111: 9}
+
 # Start reading
+while 1:
+	anData = int(io.input(pins['Aa'])) | int(io.input(pins['Ab']))<<1 | int(io.input(pins['Ac']))<<2 | int(io.input(pins['Ad']))<<3 | int(io.input(pins['Ae']))<<4 | int(io.input(pins['Af']))<<5 | int(io.input(pins['Ag']))<<6
+	catData = int(io.input(pins['CA'])) | int(io.input(pins['CB']))<<1 | int(io.input(pins['CC']))<<2
+
+	if catData == 2:
+		spo2Tens = conv[catData]
+	elif catData == 4:
+		spo2Units = conv[catData]
+	else:
+		pass
+	
+	if spo2Tens!=0 and spo2Units!=0:
+		spo2 = 10*spo2Tens + spo2Units
+	
+	print spo2
